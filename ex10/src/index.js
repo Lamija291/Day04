c6f9f214408739a5f21d9471b7ca47cf6ce5d8c2
Lamija 291 <hadroviclamija291@gmail.com>
2021-03-11 21:24:18 +0100

    added all files

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+
+    var myFirstName = "Lamija";
+    var myLastName = "Hadrovic";
+    var myHomeTown = "Sarajevo";
+
+return  {
+
+    myFirstName,
+    myLastName,
+    myHomeTown
+};
+
+
+}
+console.log(main());
+module.exports = main;
